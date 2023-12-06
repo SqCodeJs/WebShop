0fbcf20fb8d6edc2402cac6c68532809d8f74a7e
SqCodeJs <tomek.bilka@gmail.com>
2023-12-06 16:42:49 +0100

    theme: update PageWrapper

--- client/src/utils/styledComponents.ts
+++ client/src/utils/styledComponents.ts
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import { device } from "./device";
 
 const PageWrapper = styled.div`
+    width: 100%;
     margin: 0 auto;
     max-width: 1280px;
+    padding: 0 16px;
 `;
 
 const Wrapp = styled.div`
