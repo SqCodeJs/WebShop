ad848236609800a0425582da6679b80651a60a9a
SqCodeJs <tomek.bilka@gmail.com>
2023-12-06 16:59:11 +0100

    adds routes

--- client/src/routing.ts
+++ client/src/routing.ts
@@ -1,20 +1,46 @@
 import React, { lazy } from "react";
+import RequireAuth from "./auth/RequireAuth";
 const Home = lazy(() => import('./components/pages/home/Home'));
 const Listing = lazy(() => import('./components/pages/listing/Listing'));
 const Product = lazy(() => import('./components/pages/product/Product'));
+const Register = lazy(() => import('./components/Register'));
+const Login = lazy(() => import('./components/Login'));
+const Card = lazy(() => import('./components/MainCard'));
+const Error = lazy(() => import('./components/ErrorPage'));
+const UserPanel = lazy(() => import('./components/UserPanel'));
 
+const categories = ['laptops', 'electronic', 'audio', 'phones', 'console', 'watches', 'cameras'];
 const routes = [
     {
         path: '/',
         component: Home,
     },
     {
-        path: '/listing',
+        path: '/:path/:slug',
+        component: Product,
+    },
+    {
+        path: '/register',
+        component: Register
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/card',
+        component: Card
+    }, {
+        path: '/:path',
         component: Listing,
     },
     {
-        path: '/:path/:slug',
-        component: Product,
+        path: '/userpanel',
+        component: RequireAuth(UserPanel)
+    },
+    {
+        path: '/temp',
+        component: Error
     },
 ];
 
