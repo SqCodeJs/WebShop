226355ec98c5764d201bfc8703f625ed476c39c1
SqCodeJs <tomek.bilka@gmail.com>
2023-12-14 12:56:31 +0100

    feat: changes for login action

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,92 +1,100 @@
 import React, { ChangeEvent, useState } from "react";
 import styled from "styled-components";
 import { Wrapp, Container, Row, Title } from "../utils/styledComponents";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { loginAction } from "../state/actions/accountActions";
 import { RootState } from "../state/reducers/rootReducer";
 
 const WrappLogin = styled(Wrapp)`
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-
-  justify-content: stretch;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    justify-content: stretch;
 `;
+
 const ContainerLogin = styled(Container)`
-  padding: 10px 20px;
-  width: 40%;
-  justify-self: stretch;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: auto;
+    padding: 10px 20px;
+    width: 40%;
+    justify-self: stretch;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: auto;
 `;
+
 const RowLogin = styled(Row)`
-  justify-content: space-between;
-  align-items: center;
+    justify-content: space-between;
+    align-items: center;
 `;
+
 const TitleLogin = styled(Title)`
-  margin: 10px auto;
-  font-family: Open Sans, sans-serif;
-  font-size: 32px;
+    margin: 10px auto;
+    font-family: Open Sans, sans-serif;
+    font-size: 32px;
 `;
+
 const Input = styled.input`
-  width: 100%;
-  margin: 10px 0%;
-  padding: 10px;
-  font-size: 14px;
-  line-height: 24px;
-  border: 1.5px solid grey;
-  &:focus {
-    outline: none;
-  }
+    width: 100%;
+    margin: 10px 0%;
+    padding: 10px;
+    font-size: 14px;
+    line-height: 24px;
+    border: 1.5px solid grey;
+
+    &:focus {
+        outline: none;
+    }
 `;
+
 const Button = styled.button`
-  width: 60%;
-  margin: 10px 0 0 40%;
-
-  font-family: Open Sans, sans-serif;
-  letter-spacing: 1px;
-  font-size: 14px;
-  font-weight: 100;
-
-  padding: 10px;
-  border: 1.5px solid grey;
-
-  cursor: pointer;
-  &:hover {
-    background-color: #2d9ae8;
-    color: white;
-  }
+    width: 60%;
+    margin: 10px 0 0 40%;
+    font-family: Open Sans, sans-serif;
+    letter-spacing: 1px;
+    font-size: 14px;
+    font-weight: 100;
+    padding: 10px;
+    border: 1.5px solid grey;
+    cursor: pointer;
+  
+    &:hover {
+        background-color: #2d9ae8;
+        color: white;
+    }
 `;
+
 const LinkStyled = styled(Link)`
-  background-color: transparent;
-  text-decoration: none;
+      background-color: transparent;
+    text-decoration: none;
 `;
-const RegisterButton = styled(Button)`
-  width: 100%;
-  margin: 5px;
-  border: 1px solid #2d9ae8;
-  color: #2d9ae8;
-`;
-const Paragraf = styled.p`
-  font-family: Open Sans, sans-serif;
-  font-weight: 100;
-  font-size: 16px;
-  color: #767676;
-`;
-const LoginParagraf = styled(Paragraf)`
-  margin: 5px;
-  padding: 5px;
-  font-size: 20px;
-  font-weight: 300;
 
-  color: #2d9ae8;
+const RegisterButton = styled(Button)`
+    width: 100%;
+    margin: 5px;
+    border: 1px solid #2d9ae8;
+    color: #2d9ae8;
 `;
+
+const Paragraf = styled.p`
+    font-family: Open Sans, sans-serif;
+    font-weight: 100;
+    font-size: 16px;
+    color: #767676;
+`;
+
+const LoginParagraf = styled(Paragraf)`
+    margin: 5px;
+    padding: 5px;
+    font-size: 20px;
+    font-weight: 300;
+    color: #2d9ae8;
+`;
+
 const Login = () => {
     const loginMessage = useSelector((state: RootState) => state.message.loginInfo);
     const [data, setData] = useState<{ mail: string; password: string; }>({ mail: "", password: "" });
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const setFormData = (e: ChangeEvent<HTMLInputElement>) =>
         setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -111,11 +119,14 @@ const Login = () => {
                 />
 
                 <Button
-                    onClick={() => {
-                        dispatch(loginAction(data));
-                        //todo handle after login
-
-                        setData({ mail: "", password: "" });
+                    onClick={async () => {
+                        try {
+                            await dispatch(loginAction(data));
+                            setData({ mail: "", password: "" });
+                            navigate('/userpanel');
+                    } catch(e) {
+                        console.log(e)
+                    }
                     }}
                 >
                     Zaloguj się
@@ -131,7 +142,5 @@ const Login = () => {
         </WrappLogin>
     );
 };
-
-//todo add auth wrapper
 
 export default Login;
